e4deae9db470af1d716bd56e68d4ba8caf2e66de
salix <csauce295@gmail.com>
2024-09-23 19:24:53 -0500

    error dynamic made

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,14 +6,15 @@ const URL = 'http://localhost:9000/api/todos'
 export default class App extends React.Component {
   state={
     todos: [],
+    error: ''
  }
  fetchAllTodos = () => {
   axios.get(URL)
   .then(res => {
     this.setState({ ...this.state, todos: res.data.data})
   })
-  .catch(err =>{
-   debugger
+  .catch(error =>{
+    this.setState({...this.state, error: error.response.data.message})
   })
  }
  componentDidMount() {
@@ -22,7 +23,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <div id='error'>Error: No error here</div>
+        <div id='error'>ERROR: {this.state.error}</div>
         <div id='todos'>
           <h2>Todos:</h2>
           {
